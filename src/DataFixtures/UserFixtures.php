20b2561161c7882ff7b39183d6aab47a8405d402
Maxime Cornet <xelysion@icloud.com>
2019-01-27 16:33:15 +0100

    Add doctrine fixtures bundle

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use App\Factory\UserFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class UserFixtures extends Fixture
+{
+    /** @var UserFactory */
+    private $userFactory;
+
+    public function __construct(UserFactory $userFactory)
+    {
+        $this->userFactory = $userFactory;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        /** @var User $user */
+        $user = $this->userFactory->create('john@doe.fr', 'john');
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
